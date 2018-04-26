90f51a9f277dc7f48986cbbd8a8ebee13797b92a
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-25 20:24:41 -0700

    commit all

--- moreWithMethods38/Program.cs
+++ moreWithMethods38/Program.cs
@@ -10,6 +10,19 @@ namespace moreWithMethods38
     {
         static void Main(string[] args)
         {
+            string enterNum = "Enter a number: ";
+            Console.WriteLine(enterNum);
+            int inputReq = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second if you'd like (optional), otherwise enter \"0\": ");
+            int inputOpt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Thanks.");
+
+
+
+            mathOperation math = new mathOperation();
+            int functionRun = math.Divide(inputReq, inputOpt);
+            Console.WriteLine(functionRun);
+            Console.ReadLine();
         }
     }
 }
---
+++ moreWithMethods38/mathOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreWithMethods38
+{
+    class mathOperation
+    {
+        public int Divide (int i, int j = 4)
+        {
+            return (i + j) / 2;
+        }
+    }
+}
