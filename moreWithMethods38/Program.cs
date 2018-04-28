0d48d888c338211d33e0ef71847a914f28b85a97
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-27 18:54:06 -0700

    commit all

--- moreWithMethods38/Program.cs
+++ moreWithMethods38/Program.cs
@@ -15,14 +15,21 @@ namespace moreWithMethods38
             int inputReq = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a second if you'd like (optional), otherwise enter \"0\": ");
             int inputOpt = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Thanks.");
-
-
 
             mathOperation math = new mathOperation();
-            int functionRun = math.Divide(inputReq, inputOpt);
-            Console.WriteLine(functionRun);
+            if (inputOpt == 0)
+            {
+                int singleInput = math.Divide(inputReq);
+                Console.WriteLine(singleInput);
+            }
+            else
+            {
+                int doubleInput = math.Divide(inputReq, inputOpt);
+                Console.WriteLine(doubleInput);
+            }
             Console.ReadLine();
+
+
         }
     }
 }
